3195a1a9429e520b0a4d61d63a97010b0cddcc43
HaiGe <GodL9211@126.com>
2024-05-08 00:29:01 +0800

    playwright调用js脚本

--- python调用js/方式7_playwright.py
+++ python调用js/方式7_playwright.py
@@ -3,17 +3,16 @@
 from playwright.sync_api import sync_playwright
 
 if __name__ == '__main__':
-
     # 启动 Playwright
     with sync_playwright() as p:
         # 启动 Chromium 浏览器
-        browser = p.chromium.launch()
+        browser = p.chromium.launch(headless=False)
 
         # 创建新页面
         page = browser.new_page()
 
         # 打开网页
-        page.goto("about:blank")
+        page.goto("https://baidu.com")
 
         # 定义 JavaScript 代码
         js_code = """
@@ -21,14 +20,12 @@ if __name__ == '__main__':
             return a + b;
         }
         """
-
+        page.add_script_tag(content=js_code)  # 使用 page.add_script_tag() 方法将其注入到页面中，然后在页面上执行它
         # 在当前页面上执行 JavaScript 代码
-        result = page.evaluate(js_code, [3, 5])
+        result = page.evaluate("add(5, 7)")
 
         # 输出执行结果
-        print("JavaScript 执行结果:", result)
+        print("JavaScript 执行结果:", result)  # JavaScript 执行结果: 12
 
         # 关闭浏览器
         browser.close()
-
-
